0f146e483cb7fa011fe638d0c41c176311855036
Doug Jones <DSJR1284@GMAIL.COM>
2022-03-29 22:11:30 -0400

    testing game animation loop in main function

--- Snake /game.js
+++ Snake /game.js
@@ -1,7 +1,11 @@
+let lastRenderTime = 0 
+
 
 function main(currentTime) {
+    const secSinceLastRender = (currentTime - lastRenderTime) / 1000
     window.requestAnimationFrame(main)
-    console.log(currentTime) 
+    lastRenderTime = currentTime
+    console.log(secSinceLastRender) 
 }
 
 window.requestAnimationFrame(main)
