afa3117520b7ff4e19ed14f4185cfd42f6a106b6
Doug Jones <DSJR1284@GMAIL.COM>
2022-04-03 22:19:09 -0400

    add snake speed varibale and conditional code

--- Snake /game.js
+++ Snake /game.js
@@ -1,11 +1,12 @@
 let lastRenderTime = 0 
-
+const snakeSpeed = 2
 
 function main(currentTime) {
-    const secSinceLastRender = (currentTime - lastRenderTime) / 1000
     window.requestAnimationFrame(main)
-    lastRenderTime = currentTime
-    console.log(secSinceLastRender) 
+    const secSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secSinceLastRender < 1 / snakeSpeed)
+    return lastRenderTime = currentTime
+    // console.log(secSinceLastRender) 
 }
 
 window.requestAnimationFrame(main)
