176bfb2d838debee2cee789fc800bb90362b7aa6
Doug Jones <DSJR1284@GMAIL.COM>
2022-04-03 23:29:39 -0400

    set up snake and food files

--- Snake /game.js
+++ Snake /game.js
@@ -8,14 +8,19 @@ function main(currentTime) {
     window.requestAnimationFrame(main) //Tell me when I can render my next frame.
     const secSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secSinceLastRender < 1 / snakeSpeed)
-    return lastRenderTime = currentTime //updating or last render time. 
-    
+    return lastRenderTime = currentTime //updating or last render time.     
     
     update() //update logic of the game.
     render() //dispaly current state of the game based on update.
-
-
 }
 
 window.requestAnimationFrame(main)
+
+function update() {
+
+}
+
+function render() {
+
+}
 
