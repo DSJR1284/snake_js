f7889dc437e928f98f0f93c13ae1c1557ad46068
Doug Jones <DSJR1284@GMAIL.COM>
2022-04-06 22:01:16 -0400

    set up function in snake.js

--- Snake /game.js
+++ Snake /game.js
@@ -1,14 +1,17 @@
-//Game Loop and Setup file. 
+import { snakeSpeed } from './snake.js'
 
+//Game Loop and Setup file.
 let lastRenderTime = 0 
-const snakeSpeed = 2 //how many times the snake moves per sec
+
 
 
 function main(currentTime) {
     window.requestAnimationFrame(main) //Tell me when I can render my next frame.
     const secSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secSinceLastRender < 1 / snakeSpeed)
-    return lastRenderTime = currentTime //updating or last render time.     
+    return 
+    console.log('Render')
+    lastRenderTime = currentTime //updating or last render time.     
     
     update() //update logic of the game.
     render() //dispaly current state of the game based on update.
---
+++ Snake /snake.js
@@ -0,0 +1,9 @@
+export const snakeSpeed = 2 //how many times the snake moves per sec
+
+function update() {
+
+}
+
+function render() {
+    
+}
