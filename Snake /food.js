ecf87438701828503f0376d167b22d916a175348
Doug Jones <DSJR1284@GMAIL.COM>
2022-04-15 15:48:54 -0400

    add new function to snake.js imported new function to food.js

--- Snake /food.js
+++ Snake /food.js
@@ -1,9 +1,11 @@
+import { onSnake, expandSnake } from './snake.js'
+
 let food = { x: 10, y: 1 }
 const expansionRate = 1
 
 export function update() {
     if (onSnake(food)) {
-        expnadSnake(expansionRate)
+        expandSnake(expansionRate)
         food = { x: 20, y: 10 }
     }
 
--- Snake /snake.js
+++ Snake /snake.js
@@ -2,6 +2,7 @@ import { getInputDirection } from "./input.js"
 
 export const snakeSpeed = 2 
 const snakeBody = [{x: 11, y: 11}] 
+let newSegment = 0 
 
 export function update() {
     const inputDirection = getInputDirection()
@@ -20,4 +21,18 @@ export function render(gameBoard) {
     snakeElement.classList.add('snake')
     gameBoard.appendChild(snakeElement) 
     })
+}
+
+export function expandSnake(amount) {
+    newSegment += amount 
+}
+
+export function onSnake(postion) {
+    return snakeBody.some(segment => {
+        return equalPostions(segment, postion)
+    })
+}
+
+function equalPostions(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y 
 }
