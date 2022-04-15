374a90491020448cf2cb6a1a49192b335ba3f023
Doug Jones <DSJR1284@GMAIL.COM>
2022-04-15 15:38:51 -0400

    working on update function for dood, testing snake and food dispaly

--- Snake /food.js
+++ Snake /food.js
@@ -1,6 +1,11 @@
-let food = { x: 0, y: 0 }
+let food = { x: 10, y: 1 }
+const expansionRate = 1
 
 export function update() {
+    if (onSnake(food)) {
+        expnadSnake(expansionRate)
+        food = { x: 20, y: 10 }
+    }
 
 }
 
