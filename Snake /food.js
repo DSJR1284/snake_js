b07204af7b1510ad072ed585396e47637633c270
Doug Jones <DSJR1284@GMAIL.COM>
2022-04-24 23:06:50 -0400

    testing new function/ add randomGridPostion function

--- Snake /food.js
+++ Snake /food.js
@@ -1,12 +1,13 @@
 import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
 
-let food = { x: 10, y: 1 }
+let food = getRandomFood()
 const expansionRate = 1
 
 export function update() {
     if (onSnake(food)) {
         expandSnake(expansionRate)
-        food = { x: 20, y: 10 }
+        food = getRandomFood()
     }
 }
 
